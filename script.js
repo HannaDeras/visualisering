fb16e63823bc59683cbc4fb39f491f4767b4b76f
HannaDeras <120727382+HannaDeras@users.noreply.github.com>
2025-01-13 11:57:32 +0100

    svg

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
+/* import * as d3 from "https://cdn.jsdelivr.net/npm/d3@7/+esm";
 
  
 
@@ -84,7 +84,7 @@ svg.selectAll(".label")
 
 
 
-    
+     */
 
 //.attr("stroke", platform === "Instagram" ? " #833AB4" : "gray")
 
